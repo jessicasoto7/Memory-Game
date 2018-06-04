e56f9df6e0151a656399298619c45ef54aa73f18
Jessica Tirado <jessicasoto7@gmail.com>
2018-06-04 15:31:23 -0500

    added timer function on click

--- js/app.js
+++ js/app.js
@@ -43,8 +43,19 @@ let flippedCard = []; //empty array
 let matchCards = []; //empty match cards array
 
 
+
 card.forEach(function(card) {
     card.addEventListener('click', function(e) {
+      //Timer function and adapted from https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+      const time = setInterval(timer, 1000);
+      let totalSeconds = 0;
+      function timer() { // Timer starts when first card is clicked on.
+          ++totalSeconds;
+          const hour = Math.floor(totalSeconds /3600);
+          const minute = Math.floor((totalSeconds - hour*3600)/60);
+          const seconds = totalSeconds - (hour*3600 + minute*60);
+          document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+      }	
    
       if(!card.classList.contains('open', 'show', 'match')) { 
         flippedCard.push(card); //card pushes into array
