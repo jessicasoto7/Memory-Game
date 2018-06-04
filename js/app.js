b5670a1bb716dedbcfec783fa25fc386650aff71
Jessica Tirado <jessicasoto7@gmail.com>
2018-06-04 15:38:54 -0500

    removed timer function from click event due to bug. Timer now starts when newGame starts.

--- js/app.js
+++ js/app.js
@@ -41,22 +41,21 @@ function shuffle(array) {
 const card = document.querySelectorAll('.card');
 let flippedCard = []; //empty array
 let matchCards = []; //empty match cards array
+//Timer function and adapted from https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+const time = setInterval(timer, 1000);
+let totalSeconds = 0;
 
-
+function timer() { // Timer starts.
+    ++totalSeconds;
+    const hour = Math.floor(totalSeconds /3600);
+    const minute = Math.floor((totalSeconds - hour*3600)/60);
+    const seconds = totalSeconds - (hour*3600 + minute*60);
+    document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
+}	
 
 card.forEach(function(card) {
     card.addEventListener('click', function(e) {
-      //Timer function and adapted from https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-      const time = setInterval(timer, 1000);
-      let totalSeconds = 0;
-      function timer() { // Timer starts when first card is clicked on.
-          ++totalSeconds;
-          const hour = Math.floor(totalSeconds /3600);
-          const minute = Math.floor((totalSeconds - hour*3600)/60);
-          const seconds = totalSeconds - (hour*3600 + minute*60);
-          document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;
-      }	
-   
+ 
       if(!card.classList.contains('open', 'show', 'match')) { 
         flippedCard.push(card); //card pushes into array
         card.classList.add('open', 'show'); //Add classes
@@ -71,6 +70,7 @@ card.forEach(function(card) {
 
               if (matchCards.length === 8) { // 8 pairs match, you won game.
 		          alert('You Won!'); //placeholder for modal
+		          clearInterval(time);
 		          console.log('You won!');
 		          resetGame();
 	            }
